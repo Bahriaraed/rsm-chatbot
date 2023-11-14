448c3125168d05fafb77f5a1874f32ffe9a3da41
Bahriaraed <92522944+Bahriaraed@users.noreply.github.com>
2023-11-14 14:28:07 +0000

    first commit

--- Hello.py
+++ Hello.py
@@ -1,51 +1,53 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
 
-LOGGER = get_logger(__name__)
+# Placeholder for the chatbot model
+chatbot = None
 
+# Streamlit app title
+st.title('PRM Chatbot')
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+# Function to interact with the chatbot
+def chat(message):
+    if chatbot is None:
+        return "Please wait, the model is still loading..."
+    else:
+        # Pass user input to the chatbot model and get the response
+        # Example: response = chatbot.generate_response(message)
+        # Replace `generate_response` with your chatbot's response method
+        response = "Response from the chatbot goes here."
+        return response
 
-    st.write("# Welcome to Streamlit! 👋")
+# Define a form to capture user input
+form = st.form(key='my_form')
+user_input = form.text_input('Enter your message here:')
+submit_button = form.form_submit_button('Send')
 
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+# Execute JavaScript to trigger 'click' on the form submit on Enter key press
+st.markdown(
     """
-    )
+    <script>
+    const input = document.querySelector("input");
+    input.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            document.querySelector("[data-baseweb='button']").click();
+        }
+    });
+    </script>
+    """,
+    unsafe_allow_html=True,
+)
 
+# Handle form submission
+if submit_button:
+    user_message = user_input
+    if user_message:
+        # Display user input
+        st.text("You: " + user_message)
+        # Get chatbot response
+        bot_response = chat(user_message)
+        # Display chatbot response
+        st.text("Chatbot: " + bot_response)
 
-if __name__ == "__main__":
-    run()
+# Add a footer or any additional content as needed
+st.markdown('---')
+st.write('This Chatbot will help you get an idea about you Project Risk')
