f9a7975b91247c8b0ba7aa60bb34608ba73c77b6
Bahriaraed <92522944+Bahriaraed@users.noreply.github.com>
2023-11-15 14:03:38 +0000

    12 commit

--- Hello.py
+++ Hello.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from fuzzywuzzy import process
+from fuzzywuzzy import fuzz, process
 
 
 file_path = "output_df_final.csv"
@@ -8,15 +8,16 @@ data = pd.read_csv(file_path)
 
 # Function to extract sections based on the concept
 def get_section(user_input):
-    # Use fuzzy matching to find similar concepts
+    # Use fuzzy matching to find similar concepts with adjusted matching parameters
     concepts = data['Content'].tolist()
-    similar_concepts = process.extract(user_input, concepts, limit=5)  # Adjust limit as needed
+    similar_concepts = process.extract(user_input, concepts, scorer=fuzz.token_sort_ratio, limit=10)
 
     # Retrieve information for the most similar concept found
     if similar_concepts:
         most_similar_concept = similar_concepts[0][0]
         matching_row = data[data['Content'] == most_similar_concept]
         if not matching_row.empty:
+            # Extract required information
             Chapter_Name = matching_row['Chapter_Name'].iloc[0]
             Sub_chapter_Number = matching_row['Sub_chapter_Number'].iloc[0]
             Sub_chapter_Name = matching_row['Sub_chapter_Name'].iloc[0]
@@ -26,15 +27,27 @@ def get_section(user_input):
             type_val = matching_row['type'].iloc[0]
             synonyms = matching_row['synonyms'].iloc[0]
             keywords = matching_row['Keywords_Only'].iloc[0]
-            response = f"*Chapter_Name:*{Chapter_Name}\n\n*Sub_chapter_Name:*{Sub_chapter_Name}\n\n*Sub_chapter_Number:*{Sub_chapter_Number}\n\n*Sub_sub_chapter_Name:*{Sub_sub_chapter_Name}\n\n*Concept:* {concept}\n\n*Definition:* {definition}\n\n*Type:* {type_val}\n\n*Synonyms:* {synonyms}\n\n*Keywords:* {keywords}"
-            
+
+            # Constructing the response with retrieved information
+            response = (
+                f"*Chapter_Name:* {Chapter_Name}\n\n"
+                f"*Sub_chapter_Name:* {Sub_chapter_Name}\n\n"
+                f"*Sub_chapter_Number:* {Sub_chapter_Number}\n\n"
+                f"*Sub_sub_chapter_Name:* {Sub_sub_chapter_Name}\n\n"
+                f"*Concept:* {concept}\n\n"
+                f"*Definition:* {definition}\n\n"
+                f"*Type:* {type_val}\n\n"
+                f"*Synonyms:* {synonyms}\n\n"
+                f"*Keywords:* {keywords}"
+            )
+
             # Add a line to indicate the relation between user input and result
             response += f"\n\n**Result based on user input: '{user_input}'**"
         else:
             response = f"No information found for the most similar concept '{most_similar_concept}'."
     else:
         response = f"No similar concepts found for '{user_input}'."
-    
+
     return response
 
 # Define a form to capture user input
