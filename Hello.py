44a16a09e8aa48b7a260a00b13103762e4c1916f
Bahriaraed <92522944+Bahriaraed@users.noreply.github.com>
2023-11-15 13:13:09 +0000

    9 commit

--- Hello.py
+++ Hello.py
@@ -6,7 +6,6 @@ from fuzzywuzzy import process
 file_path = "output_df_final.csv"
 data = pd.read_csv(file_path)
 
-
 # Function to extract sections based on the concept
 def get_section(user_input):
     # Use fuzzy matching to find similar concepts
@@ -18,16 +17,15 @@ def get_section(user_input):
         most_similar_concept = similar_concepts[0][0]
         matching_row = data[data['Content'] == most_similar_concept]
         if not matching_row.empty:
-            Chapter_Name = matching_row['Chapter_Name'].iloc[0]
-            Sub_chapter_Name = matching_row['Sub_chapter_Name'].iloc[0]
-            Sub_sub_chapter_Name = matching_row['Sub_sub_chapter_Name'].iloc[0]
-            synonyms = matching_row['synonyms'].iloc[0]
             concept = matching_row['concepts'].iloc[0]
             definition = matching_row['Content'].iloc[0]
             type_val = matching_row['type'].iloc[0]
             synonyms = matching_row['synonyms'].iloc[0]
             keywords = matching_row['Keywords_Only'].iloc[0]
-            response = f"Concept: {concept}\nDefinition: {definition}\nType: {type_val}\nSynonyms: {synonyms}\nKeywords: {keywords}"
+            response = f"**Concept:** {concept}\n\n*Definition:* {definition}\n\n*Type:* {type_val}\n\n*Synonyms:* {synonyms}\n\n*Keywords:* {keywords}"
+            
+            # Add a line to indicate the relation between user input and result
+            response += f"\n\n**Result based on user input: '{user_input}'**"
         else:
             response = f"No information found for the most similar concept '{most_similar_concept}'."
     else:
@@ -36,21 +34,27 @@ def get_section(user_input):
     return response
 
 # Define a form to capture user input
-form = st.form(key='my_form')
-user_input = form.text_input('Enter your message here:')
-submit_button = form.form_submit_button('Send')
+def main():
+    st.title('Project Risk Management Chatbot')
+    st.write('This Chatbot will help you get an idea about your Project Risk management')
 
-# Handle form submission
-if submit_button:
-    user_message = user_input.strip().lower()  # Convert input to lowercase and remove leading/trailing spaces
-    if user_message:
-        # Display user input
-        st.text("You: " + user_input)
-        # Get response based on user input compared to the 'concept' column
-        bot_response = get_section(user_message)
-        # Display chatbot response
-        st.text("Chatbot: " + bot_response)
+    form = st.form(key='my_form')
+    user_input = form.text_input('Enter your message here:')
+    submit_button = form.form_submit_button('Send')
 
-# Add a footer or any additional content as needed
-st.markdown('---')
-st.write('This Chatbot will help you get an idea about your Project Risk management')
+    if submit_button:
+        user_message = user_input.strip().lower()
+        if user_message:
+            st.text("You: " + user_input)
+            bot_response = get_section(user_message)
+            
+            # Formatting the bot response for better readability
+            bot_response = bot_response.replace('[', '').replace(']', '').replace("'", "").replace(",", ", ")
+            bot_response = bot_response.replace('Concept:', '\nConcept:').replace('Definition:', '\nDefinition:')
+            bot_response = bot_response.replace('Type:', '\nType:').replace('Synonyms:', '\nSynonyms:')
+            bot_response = bot_response.replace('Keywords:', '\nKeywords:')
+            st.text("Chatbot: ")
+            st.write(bot_response)
+
+if __name__ == "__main__":
+    main()
